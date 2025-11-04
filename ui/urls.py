a391c57b9961092e28b6090aa94005ec751bd921
lilliebbarker <lilliebarker27@gmail.com>
2025-11-04 12:01:17 -0600

    Added "What Is FoodLens" Page

--- ui/urls.py
+++ ui/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path('', views.home, name='home')]
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('what-is-foodlens/', views.what_is_foodlens, name='what_is_foodlens'),
+]
--- ui/views.py
+++ ui/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+# Home page view
 def home(request):
     return render(request, "ui/home.html")
+
+# What is FoodLens page view
+def what_is_foodlens(request):
+    return render(request, "ui/what_is_foodlens.html")
