ce14b8b8a6bd9d9aa68805a0274cabdd6dfb80c0
chapizzy <chap.izzy@gmail.com>
2025-11-06 13:48:18 -0600

    Segmentation Functionality
    Optional prediction based on produce input with temporary dropdown bar

--- classifier/views.py
+++ classifier/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from PIL import Image
 from io import BytesIO
-from classifier.ml_models.predict import predict  # import your function
+from classifier.ml_models.predict import predict, segmented_predict, GROUPS  # import your function
 from django.views.decorators.csrf import csrf_exempt
 
 @csrf_exempt
@@ -11,8 +11,20 @@ def predict_view(request):
         uploaded_file = request.FILES["image"]  # InMemoryUploadedFile
         img = Image.open(uploaded_file).convert("RGB")
 
+        # Get top 3 predictions
         top_preds = predict(img, top_k=3)
 
-        return JsonResponse({"predictions": top_preds})
+        # If user selected a produce type, get segmented prediction
+        selected_produce = request.POST.get('produce_type')
+        segmented_result = None
 
+        if selected_produce and selected_produce in GROUPS:
+            segmented_result = segmented_predict(img, selected_produce)
+        
+        return JsonResponse({
+            "predictions": top_preds,
+            "segmented_result": segmented_result,
+            "available_produce": list(GROUPS.keys())
+        })
+    
     return JsonResponse({"error": "No image uploaded"}, status=400)
