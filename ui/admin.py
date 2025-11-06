f743d7ae8151c69a35fbd8bcc4c5bdbbe0ba2d5c
MadelineAymond <maddia0110@gmail.com>
2025-11-06 11:53:37 -0600

    Add feedback section

--- ui/admin.py
+++ ui/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Produce, Image, AnalysisResult, Feedback
 
-# Register your models here.
+
+@admin.register(Produce)
+class ProduceAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name', 'category')
+
+
+@admin.register(Image)
+class ImageAdmin(admin.ModelAdmin):
+	list_display = ('id', 'produce', 'user', 'upload_timestamp', 'status')
+
+
+@admin.register(AnalysisResult)
+class AnalysisResultAdmin(admin.ModelAdmin):
+	list_display = ('id', 'image', 'freshness_label', 'confidence_score', 'analyzed_at')
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+	list_display = ('id', 'image', 'user', 'helpful', 'session_key', 'created_at')
--- ui/urls.py
+++ ui/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('what-is-foodlens/', views.what_is_foodlens, name='what_is_foodlens'),
+    path('feedback/', views.submit_feedback, name='submit_feedback'),
 ]
